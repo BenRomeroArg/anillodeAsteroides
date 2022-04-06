422437b3b33db39a107ea5667077c7a70dfb804c
BenRomeroArg <rbenorlando@gmail.com>
2022-04-06 16:26:15 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  
+    $('#izquierda').click(function(){
+          
+        mover(2,getX('objeto'));
+    });
+    
+    $('#derecha').click(function(){ 
+        
+        mover(45 ,getX("objeto"));
+       
+    });
+      
+    $('#arriba').click(function(){
+       
+        mover(getY("objeto"),-10);
+    });
+    $('#abajo').click(function(){
+         
+          mover(getY('objeto'),45);
+    });
+   
+   
+    function mover(x,y){
+            $('#objeto').animate({top:""+y+"%",left:""+x+"%"},
+            {duration:900
+       });
+    }
+   
+    function getX(obj){
+   
+        var cord_x=$('#'+obj).css('left');
+            return cord_x;
+   
+    }
+    function getY(obj){
+        var cord_y=$('#'+obj).css('top');
+         return cord_y;
+    
+    }
+    
+  // $('#cont').hover(function () {
+         
+  //       $('#cont').animate({left:"10px",top:"200px"});
+  //       $('#cont').hide(500);
+          
+  //     }, function () {
+  //         $('#cont').animate({left: "1200px", top:"200px"});
+  //         $('#cont').show(1000);          
+  //     }
+  // );
+
+      
+
+});
