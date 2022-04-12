ef5cd2489988d258ca604b50a62e4807215b468b
BenRomeroArg <rbenorlando@gmail.com>
2022-04-12 11:37:23 -0300

    movilxz

--- app.js
+++ app.js
@@ -2,30 +2,62 @@ $(document).ready(function(){
   
     $('#izquierda').click(function(){
           
-        mover(5,getX('objeto'));
+        mover(5,getX('objeto trayecto'));
     });    
     
     $('#derecha').click(function(){ 
         
-        mover(30,getX("objeto"));
+        mover(30,getX("objeto trayecto"));
        
     });
       
     $('#arriba').click(function(){
        
-        mover(getY("objeto"),1);
+        mover(getY("objeto trayecto"),1);
     });
     $('#abajo').click(function(){
          
-          mover(getY('objeto'),25);
+          mover(getY('objeto trayecto'),25);
     });
-    
-    function mover(x,y){
-            $('#objeto').animate({top:""+y+"%",left:""+x+"%"},
-            {duration:1000
-       });
+    var arma = false;
+    $('#arma').click(function(){
+        
+        if(arma){
+            drawMina();
+            
+        }else{
+            $('#mina').remove();
+        }
+        arma = !arma;
+               
+    });
+
+    function drawMina (){
+        $('#trayecto').append('<span id="mina"></span>');
+        moverX(60,getW('mina'));
+       
     }
-   
+       
+    function mover(x,y){
+        $('#objeto').animate({top:""+y+"%",left:""+x+"%"},
+            {duration:1000
+        });
+        $('#trayecto').animate({top:""+y+"%",left:""+x+"%"},
+            {duration:1000
+        });
+       
+        
+    }
+    
+    function moverX(x,y){
+        $('#mina').animate({left:""+x+"%",top:""+y+"%"},{
+            duration:2000
+        });
+    }
+    function getW(min){
+        var direccionMina = $('#'+min).css('left');
+        return direccionMina;
+    }    
     function getX(obj){
    
         var cord_x=$('#'+obj).css('left');
